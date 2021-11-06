0ee35c2f9ba0bfa666875c74378b683a932dd4a0
other-otter <93465141+other-otter@users.noreply.github.com>
2021-11-07 02:24:21 +0800

    Update package.lisp

--- package.lisp
+++ package.lisp
@@ -1,4 +1,21 @@
 (in-package :cl-user)
 
 (defpackage #:http-string
-    (:use :cl :cl-user))
+    (:use :cl :cl-user)
+    (:documentation "https://github.com/other-otter/http-string/blob/main/README.md")
+    (:export split-string
+             read-input-string
+             function-map
+             get-field
+             add-app
+             rem-app
+             user-function-map
+             err-map
+             print-http-field
+             make-http-string
+             add-err
+             make-output-string
+             the-server-address
+             the-server-port
+             main
+             run-main))
